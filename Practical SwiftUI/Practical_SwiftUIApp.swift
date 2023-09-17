14b74897e94d3c3973e3313e1b30e201156a2466
Datafox-JCP <datafox@outlook.com>
2023-09-17 13:08:03 -0600

    Initial Commit

---
+++ Practical SwiftUI/Practical_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practical_SwiftUIApp.swift
+//  Practical SwiftUI
+//
+//  Created by Juan Hernandez Pazos on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Practical_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
